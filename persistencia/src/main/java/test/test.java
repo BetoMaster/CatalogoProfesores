b44812aa1814f8c9c26d59d34fc851682a597e0a
Adrian Arreguin <lopez.adrian89@uabc.edu.mx>
2023-09-19 15:16:33 -0700

    negocio

--- persistencia/src/main/java/test/test.java
+++ persistencia/src/main/java/test/test.java
@@ -2,7 +2,9 @@ package test;
 
 import java.util.ArrayList;
 import java.util.List;
+import mx.cimadevs.DAO.ProfesorDAO;
 import mx.cimadevs.DAO.UsuarioDAO;
+import mx.cimadevs.entidad.Profesor;
 import mx.cimadevs.entidad.Usuario;
 
 /**
@@ -12,8 +14,15 @@ import mx.cimadevs.entidad.Usuario;
 public class test {
     public static void main(String[] args) {
         List<Usuario> listaUsuarios = new ArrayList();
+        List<Profesor> listaProfesores = new ArrayList();
+        ProfesorDAO profesorDao = new ProfesorDAO();
         UsuarioDAO usuarioDao = new UsuarioDAO();
+        listaProfesores = profesorDao.findAll();
         listaUsuarios = usuarioDao.findAll();
+        
+        for(Profesor pr : listaProfesores){
+            System.out.println("Profesor:"+pr.getNombre()+ " " + pr.getApellido());
+        }
         
         for(Usuario us : listaUsuarios){
             System.out.println("Correo: " + us.getCorreo());
