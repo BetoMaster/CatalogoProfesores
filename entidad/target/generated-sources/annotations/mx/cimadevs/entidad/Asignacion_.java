53b588af164a2d603ef5a69cc94328097e49a558
Adrian Arreguin <lopez.adrian89@uabc.edu.mx>
2023-09-19 16:23:08 -0700

    vista dependences

--- entidad/target/generated-sources/annotations/mx/cimadevs/entidad/Asignacion_.java
+++ entidad/target/generated-sources/annotations/mx/cimadevs/entidad/Asignacion_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.StaticMetamodel;
 import mx.cimadevs.entidad.Materia;
 import mx.cimadevs.entidad.Profesor;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2023-09-18T15:03:27")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2023-09-18T15:30:31")
 @StaticMetamodel(Asignacion.class)
 public class Asignacion_ { 
 
