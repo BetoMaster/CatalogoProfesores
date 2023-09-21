4999d5e0005b66d261cd5417cebff07f6bf20b95
Adrian Arreguin <lopez.adrian89@uabc.edu.mx>
2023-09-21 13:24:29 -0700

    fix

---
+++ entidad/target/generated-sources/annotations/mx/cimadevs/entidad/Materia_.java
@@ -0,0 +1,21 @@
+package mx.cimadevs.entidad;
+
+import java.util.Date;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.ListAttribute;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+import mx.cimadevs.entidad.Asignacion;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2023-09-21T13:01:47")
+@StaticMetamodel(Materia.class)
+public class Materia_ { 
+
+    public static volatile SingularAttribute<Materia, Date> horasLaboratorio;
+    public static volatile SingularAttribute<Materia, String> nombreDeLaMateria;
+    public static volatile ListAttribute<Materia, Asignacion> asignacionList;
+    public static volatile SingularAttribute<Materia, Date> horasClase;
+    public static volatile SingularAttribute<Materia, Integer> idmateria;
+    public static volatile SingularAttribute<Materia, Date> horasTaller;
+
+}
---
+++ entidad/target/generated-sources/annotations/mx/cimadevs/entidad/Profesor_.java
@@ -0,0 +1,19 @@
+package mx.cimadevs.entidad;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.ListAttribute;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+import mx.cimadevs.entidad.Asignacion;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2023-09-21T13:01:47")
+@StaticMetamodel(Profesor.class)
+public class Profesor_ { 
+
+    public static volatile SingularAttribute<Profesor, String> apellido;
+    public static volatile ListAttribute<Profesor, Asignacion> asignacionList;
+    public static volatile SingularAttribute<Profesor, String> nombre;
+    public static volatile SingularAttribute<Profesor, Integer> idprofesor;
+    public static volatile SingularAttribute<Profesor, String> rfc;
+
+}
