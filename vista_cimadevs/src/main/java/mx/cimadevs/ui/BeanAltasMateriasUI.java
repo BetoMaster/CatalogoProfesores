b3fc0825c438b4f197106f8e9269d1781d3d4a19
Adrian Arreguin <lopez.adrian89@uabc.edu.mx>
2023-09-25 00:53:33 -0700

    Detalles

--- vista_cimadevs/src/main/java/mx/cimadevs/ui/BeanAltasMateriasUI.java
+++ vista_cimadevs/src/main/java/mx/cimadevs/ui/BeanAltasMateriasUI.java
@@ -12,15 +12,16 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 import javax.faces.model.SelectItem;
-import mx.cimadevs.entidad.Asignacion;
-import mx.cimadevs.entidad.Profesor;
 
 @ManagedBean(name = "materiaAltasBean")
 @ViewScoped
+//Esto significa que la instancia del bean estará disponible para la vista y mantendrá su estado durante la duración de la vista.
 public class BeanAltasMateriasUI implements Serializable {
 
+    // Aquí se declaran las propiedades que el bean va a utilizar para almacenar los datos ingresados en el formulario.
     private String nombreDeLaMateria;
     private String horasClase;
     private String horasTaller;
@@ -30,6 +31,7 @@ public class BeanAltasMateriasUI implements Serializable {
     private List<SelectItem> materiasSelectItems;
     private Integer materiaSeleccionada;
 
+    // Método de inicialización ejecutado después de la creación del bean, sirve para cargar la lista de materias para las asignaciones
     @PostConstruct
     public void init() {
         // Cargar la lista de materias disponibles
@@ -42,6 +44,7 @@ public class BeanAltasMateriasUI implements Serializable {
         }
     }
 
+    // Getters y setters para las propiedades del bean
     public String getNombreDeLaMateria() {
         return nombreDeLaMateria;
     }
@@ -85,19 +88,57 @@ public class BeanAltasMateriasUI implements Serializable {
     public void setMateriaSeleccionada(Integer materiaSeleccionada) {
         this.materiaSeleccionada = materiaSeleccionada;
     }
+    // Fin de los Getters y setters
 
-    // Método para guardar la materia en la base de datos.
-    public void guardarMateria() {
+    // Validación de campos obligatorios, Si alguno de los campos obligatorios está vacío, se agrega un mensaje de error a la vista.
+    private boolean validarCampos() {
+        boolean camposValidos = true;
 
-        nuevaMateria.setNombreDeLaMateria(nombreDeLaMateria);
-        nuevaMateria.setHorasClase(parseStringToDate(horasClase));
-        nuevaMateria.setHorasTaller(parseStringToDate(horasTaller));
-        nuevaMateria.setHorasLaboratorio(parseStringToDate(horasLaboratorio));
+        if (nombreDeLaMateria == null || nombreDeLaMateria.isEmpty()) {
+            FacesContext.getCurrentInstance().addMessage("globalMessages",
+                    new FacesMessage(FacesMessage.SEVERITY_ERROR, "El campo Nombre es obligatorio", null));
+            camposValidos = false;
+        }
 
-        materiaDao.save(nuevaMateria);
-        limpiarCampos();
+        if (horasClase == null || horasClase.isEmpty()) {
+            FacesContext.getCurrentInstance().addMessage("globalMessages",
+                    new FacesMessage(FacesMessage.SEVERITY_ERROR, "El campo Horas de Clase es obligatorio", null));
+            camposValidos = false;
+        }
+
+        if (horasTaller == null || horasTaller.isEmpty()) {
+            FacesContext.getCurrentInstance().addMessage("globalMessages",
+                    new FacesMessage(FacesMessage.SEVERITY_ERROR, "El campo Horas de Taller es obligatorio", null));
+            camposValidos = false;
+        }
+        if (horasLaboratorio == null || horasLaboratorio.isEmpty()) {
+            FacesContext.getCurrentInstance().addMessage(null,
+                    new FacesMessage(FacesMessage.SEVERITY_ERROR, "El campo Horas de Laboratorio es obligatorio", null));
+            camposValidos = false;
+        }
+
+        return camposValidos;
     }
 
+    /* Método para guardar la materia en la base de datos. ste método se llama cuando se presiona el botón "Guardar" en la vista. 
+    Primero, llama a validarCampos() para asegurarse de que los campos estén llenos. Si los campos son válidos, crea una nueva 
+    instancia de Materia con los datos ingresados y la guarda en la base de datos a través de materiaDao. Luego, muestra un mensaje de éxito */
+    public void guardarMateria() {
+        if (validarCampos()) {
+            nuevaMateria.setNombreDeLaMateria(nombreDeLaMateria);
+            nuevaMateria.setHorasClase(parseStringToDate(horasClase));
+            nuevaMateria.setHorasTaller(parseStringToDate(horasTaller));
+            nuevaMateria.setHorasLaboratorio(parseStringToDate(horasLaboratorio));
+
+            materiaDao.save(nuevaMateria);
+            FacesContext.getCurrentInstance().addMessage(null,
+                    new FacesMessage(FacesMessage.SEVERITY_INFO, "Registro exitoso", null));
+            limpiarCampos();
+
+        }
+    }
+
+    // Método para limpiar los campos del formulario
     public void limpiarCampos() {
 
         nombreDeLaMateria = "";
@@ -106,7 +147,8 @@ public class BeanAltasMateriasUI implements Serializable {
         horasLaboratorio = "";
     }
 
-    // Método para convertir una cadena de texto en un objeto Date
+    /* Método para convertir una cadena de texto en un objeto Date,  
+    Este método convierte una cadena de texto en un objeto Date. Se utiliza para convertir las horas ingresadas en formato HH:mm a objetos Date.*/
     private Date parseStringToDate(String texto) {
         try {
             SimpleDateFormat sdf = new SimpleDateFormat("HH:mm"); // Formato de hora
--- vista_cimadevs/src/main/java/mx/cimadevs/ui/BeanAltasUI.java
+++ vista_cimadevs/src/main/java/mx/cimadevs/ui/BeanAltasUI.java
@@ -1,8 +1,12 @@
 package mx.cimadevs.ui;
 
 import java.io.Serializable;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import mx.cimadevs.DAO.ProfesorDAO;
 import mx.cimadevs.DAO.AsignacionDAO;
 import mx.cimadevs.entidad.Asignacion;
@@ -12,19 +16,32 @@ import mx.cimadevs.entidad.Profesor;
 @ManagedBean(name = "profesorAltasBean")
 @ViewScoped
 public class BeanAltasUI implements Serializable {
+    // Aquí se declaran las propiedades que el bean va a utilizar para almacenar los datos ingresados en el formulario.
 
+    @NotNull(message = "El campo Nombre es obligatorio")
+    @Size(min = 1, message = "El campo Nombre es obligatorio")
     private String nombre;
+
+    @NotNull(message = "El campo Apellido es obligatorio")
+    @Size(min = 1, message = "El campo Apellido es obligatorio")
     private String apellido;
+
+    @NotNull(message = "El campo RFC es obligatorio")
+    @Size(min = 1, message = "El campo RFC es obligatorio")
     private String rfc;
+
     Profesor nuevoProfesor = new Profesor();
     ProfesorDAO profeDao = new ProfesorDAO();
     Integer idProfesorRegistrado;
     private Integer materiaSeleccionada;
+    private boolean mostrarDialogo;
 
+    // Métodos de inicialización
     public void init() {
 
     }
 
+    //Getters y setters
     public String getNombre() {
         return nombre;
     }
@@ -65,27 +82,72 @@ public class BeanAltasUI implements Serializable {
         this.materiaSeleccionada = materiaSeleccionada;
     }
 
-    // Método para guardar el profesor en la base de datos.
-    public void guardarProfesor() {
-        nuevoProfesor.setNombre(nombre);
-        nuevoProfesor.setApellido(apellido);
-        nuevoProfesor.setRfc(rfc);
-
-        profeDao.save(nuevoProfesor);
-
-        idProfesorRegistrado = nuevoProfesor.getIdprofesor();
-
-        // Crea una nueva asignación
-        Asignacion nuevaAsignacion = new Asignacion();
-        nuevaAsignacion.setIdprofesor(new Profesor(idProfesorRegistrado)); // Usar el ID del profesor
-        nuevaAsignacion.setIdmateria(new Materia(materiaSeleccionada)); // Usar la materia seleccionada
-
-        AsignacionDAO asignacionDao = new AsignacionDAO();
-        asignacionDao.save(nuevaAsignacion);
-
-        limpiarCampos();
+    // Getters y setters adicionales para controlar el diálogo
+    public boolean isMostrarDialogo() {
+        return mostrarDialogo;
     }
 
+    public void setMostrarDialogo(boolean mostrarDialogo) {
+        this.mostrarDialogo = mostrarDialogo;
+    }
+
+    // Método para guardar el profesor en la base de datos.
+    public void guardarProfesor() {
+        if (validarCampos()) {
+            nuevoProfesor.setNombre(nombre);
+            nuevoProfesor.setApellido(apellido);
+            nuevoProfesor.setRfc(rfc);
+
+            profeDao.save(nuevoProfesor);
+            mostrarDialogo = true;
+
+            idProfesorRegistrado = nuevoProfesor.getIdprofesor();
+
+            // Crea una nueva asignación, esta se hace por primera vez cada que se registra un nuevo profesor
+            Asignacion nuevaAsignacion = new Asignacion();
+            nuevaAsignacion.setIdprofesor(new Profesor(idProfesorRegistrado)); // Usar el ID del profesor
+            nuevaAsignacion.setIdmateria(new Materia(materiaSeleccionada)); // Usar la materia seleccionada
+
+            AsignacionDAO asignacionDao = new AsignacionDAO();
+            asignacionDao.save(nuevaAsignacion);
+            FacesContext.getCurrentInstance().addMessage(null,
+                    new FacesMessage(FacesMessage.SEVERITY_INFO, "Registro exitoso", null));
+
+            limpiarCampos();
+        }
+    }
+
+    // Método para validar los campos obligatorios
+    private boolean validarCampos() {
+        boolean camposValidos = true;
+
+        if (nombre == null || nombre.isEmpty()) {
+            FacesContext.getCurrentInstance().addMessage("globalMessages",
+                    new FacesMessage(FacesMessage.SEVERITY_ERROR, "El campo Nombre es obligatorio", null));
+            camposValidos = false;
+        }
+
+        if (apellido == null || apellido.isEmpty()) {
+            FacesContext.getCurrentInstance().addMessage("globalMessages",
+                    new FacesMessage(FacesMessage.SEVERITY_ERROR, "El campo Apellido es obligatorio", null));
+            camposValidos = false;
+        }
+
+        if (rfc == null || rfc.isEmpty()) {
+            FacesContext.getCurrentInstance().addMessage("globalMessages",
+                    new FacesMessage(FacesMessage.SEVERITY_ERROR, "El campo RFC es obligatorio", null));
+            camposValidos = false;
+        }
+        if (materiaSeleccionada == null || materiaSeleccionada <= 0) {
+            FacesContext.getCurrentInstance().addMessage(null,
+                    new FacesMessage(FacesMessage.SEVERITY_ERROR, "Debes seleccionar una materia", null));
+            camposValidos = false;
+        }
+
+        return camposValidos;
+    }
+    
+    // Método para limpiar los campos del formulario
     public void limpiarCampos() {
         nombre = "";
         apellido = "";
