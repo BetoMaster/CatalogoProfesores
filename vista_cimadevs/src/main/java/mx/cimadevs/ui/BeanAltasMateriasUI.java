c7375f704033abed9eda358472b4a34820517037
Adrian Arreguin <lopez.adrian89@uabc.edu.mx>
2023-09-29 16:46:20 -0700

    Detalles

--- vista_cimadevs/src/main/java/mx/cimadevs/ui/BeanAltasMateriasUI.java
+++ vista_cimadevs/src/main/java/mx/cimadevs/ui/BeanAltasMateriasUI.java
@@ -3,8 +3,6 @@ package mx.cimadevs.ui;
 import java.io.Serializable;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
-import mx.cimadevs.DAO.MateriaDAO;
-
 import mx.cimadevs.entidad.Materia;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -12,7 +10,6 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import javax.annotation.PostConstruct;
-import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 import javax.faces.model.SelectItem;
 import mx.cimadevs.entidad.Asignacion;
@@ -21,7 +18,6 @@ import mx.cimadevs.helper.MateriaHelper;
 
 @ManagedBean(name = "materiaAltasBean")
 @ViewScoped
-//Esto significa que la instancia del bean estará disponible para la vista y mantendrá su estado durante la duración de la vista.
 public class BeanAltasMateriasUI implements Serializable {
     
     private MateriaHelper matHelp;
@@ -33,7 +29,6 @@ public class BeanAltasMateriasUI implements Serializable {
     private List<SelectItem> materiasSelectItems;
     private Integer materiaSeleccionada;
 
-    // Método de inicialización ejecutado después de la creación del bean, sirve para cargar la lista de materias para las asignaciones
     @PostConstruct
     public void init() {
         // Cargar la lista de materias disponibles
@@ -48,7 +43,6 @@ public class BeanAltasMateriasUI implements Serializable {
         
     }
 
-    // Getters y setters para las propiedades del bean
     public String getNombreDeLaMateria() {
         return nombreDeLaMateria;
     }
@@ -92,11 +86,9 @@ public class BeanAltasMateriasUI implements Serializable {
     public void setMateriaSeleccionada(Integer materiaSeleccionada) {
         this.materiaSeleccionada = materiaSeleccionada;
     }
-    // Fin de los Getters y setters
 
-    // Validación de campos obligatorios, Si alguno de los campos obligatorios está vacío, se agrega un mensaje de error a la vista.
-    private boolean validarCampos() {
-        boolean camposValidos = true;
+    // Método para guardar la materia en la base de datos.
+    public void guardarMateria() {
 
         nuevaMateria.setNombreDeLaMateria(nombreDeLaMateria);
         nuevaMateria.setHorasClase(parseStringToDate(horasClase));
@@ -108,25 +100,6 @@ public class BeanAltasMateriasUI implements Serializable {
         limpiarCampos();
     }
 
-    /* Método para guardar la materia en la base de datos. ste método se llama cuando se presiona el botón "Guardar" en la vista. 
-    Primero, llama a validarCampos() para asegurarse de que los campos estén llenos. Si los campos son válidos, crea una nueva 
-    instancia de Materia con los datos ingresados y la guarda en la base de datos a través de materiaDao. Luego, muestra un mensaje de éxito */
-    public void guardarMateria() {
-        if (validarCampos()) {
-            nuevaMateria.setNombreDeLaMateria(nombreDeLaMateria);
-            nuevaMateria.setHorasClase(parseStringToDate(horasClase));
-            nuevaMateria.setHorasTaller(parseStringToDate(horasTaller));
-            nuevaMateria.setHorasLaboratorio(parseStringToDate(horasLaboratorio));
-
-            materiaDao.save(nuevaMateria);
-            FacesContext.getCurrentInstance().addMessage(null,
-                    new FacesMessage(FacesMessage.SEVERITY_INFO, "Registro exitoso", null));
-            limpiarCampos();
-
-        }
-    }
-
-    // Método para limpiar los campos del formulario
     public void limpiarCampos() {
 
         nombreDeLaMateria = "";
@@ -135,8 +108,7 @@ public class BeanAltasMateriasUI implements Serializable {
         horasLaboratorio = "";
     }
 
-    /* Método para convertir una cadena de texto en un objeto Date,  
-    Este método convierte una cadena de texto en un objeto Date. Se utiliza para convertir las horas ingresadas en formato HH:mm a objetos Date.*/
+    // Método para convertir una cadena de texto en un objeto Date
     private Date parseStringToDate(String texto) {
         try {
             SimpleDateFormat sdf = new SimpleDateFormat("HH:mm"); // Formato de hora
